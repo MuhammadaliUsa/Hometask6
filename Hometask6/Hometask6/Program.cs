129fb46d25a2c075e0ff4bc85b932539a86ad03c
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-06-05 17:33:09 +0500

    second commit

--- Hometask6/Hometask6/Program.cs
+++ Hometask6/Hometask6/Program.cs
@@ -4,10 +4,15 @@
     {
         static void Main(string[] args)
         {
-            string[] directories = Directory.GetDirectories("C:\\New folder\\","book*");
+            string[] directories = Directory.GetDirectories("C:\\New folder\\","bin*");
             foreach(string d in directories) 
             {
                 Console.WriteLine(d);
+            }
+            string[] directories1 = Directory.GetDirectories("C:\\New folder\\", "obj*");
+            foreach (string d1 in directories1)
+            {
+                Console.WriteLine(d1);
             }
         }
     }
