fdc0759a2618fb9094b63ac392b726ab129066b3
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-06-05 16:37:10 +0500

    Task 1 finished

--- Hometask6/Hometask6/Program.cs
+++ Hometask6/Hometask6/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string file = "C:\\New folder\\.gitignore";
+            if (File.Exists(file))
+            {
+                Console.WriteLine("has");
+            }
+            else
+            {
+                File.Create(file);
+            }
         }
     }
 }
